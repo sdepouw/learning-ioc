8ab57f271e1e8771032512f1dd84216fb359daf8
Scott DePouw <scott.depouw@gmail.com>
2014-06-02 11:26:49 -0400

    Using dependency resolution in MVC project on the About view.

--- UIs/MVC/Controllers/HomeController.cs
+++ UIs/MVC/Controllers/HomeController.cs
@@ -1,13 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.Linq;
 using System.Web.Mvc;
+using LearningNinject.Core.Interfaces.External;
+using LearningNinject.DependencyResolution;
 
 namespace MVC.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly IWidgetRepository widgetRepository = DependencyResolverInstance.Resolver.Resolve<IWidgetRepository>();
+        private readonly IPaymentService paymentService = DependencyResolverInstance.Resolver.Resolve<IPaymentService>();
+
         public ActionResult Index()
         {
             return View();
@@ -15,7 +17,11 @@ namespace MVC.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            // Let's make a payment when the user visits the about page!
+            string myWidget = widgetRepository.FetchWidgets().Last();
+            paymentService.MakePaymentForWidget(myWidget);
+
+            ViewBag.Message = string.Format("Payment made for the '{0}' widget!", myWidget);
 
             return View();
         }
